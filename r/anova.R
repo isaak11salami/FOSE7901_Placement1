f4103dff891d530157717d86e822a21f8095bbd7
isaak11salami <isaak.salami@students.mq.edu.au>
2025-04-13 19:21:23 +1000

    anova for weights done

---
+++ r/anova.R
@@ -0,0 +1,39 @@
+#Let's conduct an anova to look for differences in average weight between 
+#treatment groups
+groupedweights <- read_csv("weightsforaov.csv")
+View(groupedweights)
+groupedweights
+
+
+#Let's start with a ONE-Way ANOVA (only consider nodulation group)
+
+nod.model=lm(weight~group, groupedweights)
+nod.model
+
+#Test the model for ANOVA assumptions
+#1) Homogeneity of Variance, via...
+#Levene Test
+leveneTest(nod.model)
+#Plot Residuals against Fitted values
+plot(nod.model)
+#Look to the Q-Q plot to assess normality of variance 
+
+#Mostly okay, though not perfect. Let's run the ANOVA and see how it looks
+anova(nod.model)
+
+#Let's do another ONE-Way, but now sort by species
+sp.model=lm(weight~species, groupedweights)
+sp.model
+#Test the assumptions
+#Levene Test
+leveneTest(sp.model)
+#Plot Residuals against Fitted values
+plot(sp.model)
+#Look to the Q-Q plot to assess normality of variance 
+
+#Again, just mostly okay. Run the ANOVA anyway
+anova(sp.model)
+
+
+
+
